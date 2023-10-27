d0f25d39f4724de48023a618c788606eeb32083e
Ankit Techsopi <ankit.tiwari@techsopi.com>
2023-10-27 14:37:36 +0530

    hello world file added

---
+++ src/main/java/HellloWorld1.java
@@ -0,0 +1,20 @@
+package main.java;
+
+public class HellloWorld1 {
+
+	public static void main(String[] args) {
+		
+	    String s = "Hello from pull";
+		int i = 10;
+		int j  = 12;
+		int k = 100;
+
+		int c = 69;
+
+		String msg = "checking for the conflict";
+
+   System.out.println("abc ");
+
+	}
+
+}
