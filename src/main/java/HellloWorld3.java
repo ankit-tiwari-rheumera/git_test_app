d108f30c843091e407138799a39f8d7f92eedc17
Ankit Techsopi <ankit.tiwari@techsopi.com>
2023-11-03 17:07:00 +0530

    testing the commands

---
+++ src/main/java/HellloWorld3.java
@@ -0,0 +1,39 @@
+package main.java;
+
+public class HellloWorld1 {
+
+	public static void main(String[] args) {
+		
+	 
+
+Integer sb = 500;
+
+		String abc = "CONFLICT...";
+		
+
+		String msg = "checking for the conflict";
+
+   System.out.println("abc ");
+
+//to do work
+String s = "Hello from pull";
+		int i = 10;
+		int j  = 12;
+		int k = 100;
+
+		int c = 69;
+
+	}
+
+@Test
+	public void testAdd() {
+		obj.add("ronak");
+		obj.add("prasant");
+		obj.add("aniska");
+		obj.add("ayushi");
+
+		assertEquals(4, obj.sizeOfStudent());
+	}
+	
+
+}
