c2a3315ed8c9c549717113168934eb7f5966b7c7
Ankit Techsopi <ankit.tiwari@techsopi.com>
2023-11-03 14:51:22 +0530

    using merge and rebase

---
+++ src/main/java/HellloWorld2.java
@@ -0,0 +1,53 @@
+package main.java;
+
+public class HellloWorld1 {
+
+	public static void main(String[] args) {
+		
+	    String s = "Hello from pull";
+		int i = 10;
+		int j  = 12;
+		int k = 100;
+
+		int c = 69;
+
+Integer sb = 500;
+
+		String abc = "CONFLICT...";
+		
+
+		String msg = "checking for the conflict";
+
+   System.out.println("abc ");
+
+//to do work
+String s = "Hello from pull";
+		int i = 10;
+		int j  = 12;
+		int k = 100;
+
+		int c = 69;
+
+	}
+
+@Test
+	public void testAdd() {
+		obj.add("ronak");
+		obj.add("prasant");
+		obj.add("aniska");
+		obj.add("ayushi");
+
+		assertEquals(4, obj.sizeOfStudent());
+	}
+	@Test
+	public void testAdd() {
+		obj.add("ronak");
+		obj.add("prasant");
+		obj.add("aniska");
+		obj.add("ayushi");
+
+		assertEquals(4, obj.sizeOfStudent());
+	}
+
+
+}
